2b7e8c480731b13bbb9e3a4ccb871220b1a1e5c9
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-09-24 18:33:16 -0700

    JavaScript recap

--- 025-mini-timer.js
+++ 025-mini-timer.js
@@ -17,3 +17,5 @@ const timer = setInterval(function() {
 
 
 
+
+
---
+++ 036-js-react-tutorial-tryout-file.js
@@ -0,0 +1,66 @@
+class Human {
+    constructor() {
+        this.gender = 'Male';
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = 'Max';
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+
+const arr = [1,2,3,4,5];
+console.log(arr); 
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(`Iteration ${i + 1}: ${arr[i]}`);
+}
+
+const newArr = [...arr, 10, 20];
+console.log(newArr);
+
+newArr.forEach(e => {
+    console.log(`Element content is: ${e}`);
+});
+
+// Spread
+const p = {
+    name: 'Jieqing'
+}
+
+const pNew = {
+    ...p,
+    age: 20
+}
+
+console.log(pNew);
+
+// Rest - Merge the operator in to an array
+const filter = (...args) => {
+    return args.filter(el => el === 1);
+}
+
+console.log(filter(1, 2, 3));
+
+
+
+
+
+
+
+
+
+
+
