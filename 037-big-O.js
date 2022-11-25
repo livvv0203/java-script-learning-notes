1f7b7659a78a9c1a72fb7180d01399912fbd1ef9
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-11-25 10:34:15 -0800

    recursion recap

--- 037-big-O.js
+++ 037-big-O.js
@@ -1,6 +1,6 @@
 function addUp() {
     let total = 0;
-    for (let i = 1; i < 1000000000; i++) {
+    for (let i = 1; i < 10000; i++) {
         total = total + 1;
     }
     return total;
--- 038-algorithms.js
+++ 038-algorithms.js
@@ -1,23 +1,51 @@
-// Objects
-var obj = [
-    {
-        firstName: "Livvv",
-        isInstructor: true,
-        favNum: [32, 34, 56]
-    },
-    {
-        firstName: "Olivia2",
-        isInstructor: false,
-        favNum: [32, 34, 56]
-    },
-]
+// Recursion - countDown Recursively
+function countDownRecur(num) {
+  if (num <= 0) {
+    console.log("Finished!");
+    return;
+  } else {
+    console.log(num);
+    num--;
+    countDownRecur(num);
+  }
+}
 
-console.log(obj.keys);
+// countDownRecur(10);
 
+// Iteratively
+function countDownIteratively(num) {
+  for (var i = num; i > 0; i--) {
+    console.log(i);
+  }
+  console.log("Done!");
+}
 
+// countDownIteratively(5);
 
+// 2nd Recursion function
+function sumRange(num) {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
 
+// Factorial
+// 4 * 3 * 2 * 1
+function fac(num) {
+  let total = 1;
+  for (let i = num; i > 1; i--) {
+    total = total * i;
+  }
+  console.log(total);
+  return total;
+}
 
+// fac(6);
+// Recursively - Fac
+function facRecur(num) {
+  if (num === 1) return 1;
 
+  return num * facRecur(num - 1);
+}
 
-
+const result = facRecur(6);
+console.log(result);
